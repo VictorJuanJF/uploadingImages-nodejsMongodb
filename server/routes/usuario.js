0d48c1fea5cf05e36208fa33a9a379eabba3ae21
VictorJuanJF <vj.jimenez96@gmail.com>
2018-09-11 23:39:20 -0500

    ergre

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const bodyParser = require('body-parser');
 const Usuario = require('../models/usuario');
@@ -46,7 +45,7 @@ app.post('/usuario', (req, res) => {
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
+        password: body.password,
         role: body.role
     });
 
